98e2a7945e5fe72069ae06ffbae16e7106d12f41
donghuaxiao <x_donghua@163.com>
2019-05-13 23:05:49 +0800

    project init

---
+++ src/main/java/com/xtu/netty/App.java
@@ -0,0 +1,15 @@
+package com.xtu.netty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Logger LOG = LoggerFactory.getLogger(App.class);
+		LOG.info("hello world");
+	}
+}
